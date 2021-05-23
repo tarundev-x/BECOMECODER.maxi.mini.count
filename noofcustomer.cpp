81fd3698337f7969ecb8518df774f0a995b1d842
tarundev-x <62273965+tarundev-x@users.noreply.github.com>
2021-05-23 11:54:47 -0700

    Update noofcustomer.cpp

--- noofcustomer.cpp
+++ noofcustomer.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main()
 {
     int n,count;
-    int a[n];
+    int a[10];
     int idno;
     cout<<"Enter the number of customer"<<endl;
     cin>>n;
     cout<<"Enter the customer id:";
-    for(int i=0;i<n;i++)
+    for(int i=0;i<10;i++)
     {
         cin>>a[i];
     }
-    for(int i=0;i<n;i++)
+    for(int i=0;i<10;i++)
     {
     	count=0;
-    	for(int j=1;j<n;j++)
+    	for(int j=1;j<10;j++)
     	{
     		if(a[i]==a[j])
 	 {
