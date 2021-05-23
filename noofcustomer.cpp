2ded4f615b5d57c22683579bae1c1d706cc92ce0
tarundev-x <62273965+tarundev-x@users.noreply.github.com>
2021-05-24 00:19:58 +0530

    Add files via upload

---
+++ noofcustomer.cpp
@@ -0,0 +1,35 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count;
+    int a[n];
+    int idno;
+    cout<<"Enter the number of customer"<<endl;
+    cin>>n;
+    cout<<"Enter the customer id:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int maxi=INT_MIN;//a[0]
+    for(int i=0;i<n;i++)
+    {
+    	count=0;
+    	for(int j=1;j<n;j++)
+    	{
+    		if(a[i]==a[j])
+	 {
+	 count++;	
+		}
+	}	
+	if(count>0)
+	{
+	idno=a[i];
+	}
+	
+	}
+	cout<<"customer id:"<<idno<<" count="<<count;
+
+}
